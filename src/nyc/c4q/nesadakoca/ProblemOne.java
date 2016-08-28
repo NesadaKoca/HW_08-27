18c24b67d8e95f71186cfcb672fe2b3501752745
Nesada Koca <knesada@yahoo.com>
2016-08-28 17:27:57 -0400

    Add Problems 1-5

---
+++ src/nyc/c4q/nesadakoca/ProblemOne.java
@@ -0,0 +1,24 @@
+package nyc.c4q.nesadakoca;
+
+/**
+ * Created by nesadakoca on 8/28/16.
+ */
+//Write a method named add that takes in two numbers as arguments.
+// The function should return the sum of the two numbers.
+//
+//
+public class ProblemOne {
+    public static void main (String[] args){
+
+        System.out.println(add(1,2)); // retun 3
+    }
+
+    public static int add(int a, int b){
+        int result;
+        result = a + b;
+
+        return result;
+
+
+    }
+}
---
+++ src/nyc/c4q/nesadakoca/ProblemThree.java
@@ -0,0 +1,42 @@
+package nyc.c4q.nesadakoca;
+
+/**
+ * Created by nesadakoca on 8/28/16.
+ */
+//Check to see if a string has the same count of 'x's and 'o's. The method must return a boolean and be case insensitive.
+//The string may contain any Unicode character -- not just 'x's and 'o's -- and may be of any length.
+
+public class ProblemThree {
+    public static void main(String[] args){
+        System.out.println(sameXo("xooxx"));
+        System.out.println(sameXo("xooxx"));
+        System.out.println(sameXo("ooxXm"));
+        System.out.println(sameXo("zpzpzpp"));
+        System.out.println(sameXo("zzoo"));
+
+    }
+    public static boolean sameXo(String xo){
+        boolean isSameCount = false;
+        int totalX = 0;
+        int totalO = 0;
+
+        for(int i = 0; i < xo.length(); i++) {
+
+
+            if (Character.toLowerCase(xo.charAt(i)) == 'x') {
+                totalX++;
+
+
+            }
+            if (Character.toLowerCase(xo.charAt(i)) == 'o') {
+                totalO++;
+
+            }
+        }
+
+        return totalX == totalO;
+    }
+
+
+}
+
